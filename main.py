9bd26078a97dbd13fe5911c7422a9c1a4a8264bf
thomas-dubard <55085630+thomas-dubard@users.noreply.github.com>
2019-10-02 15:09:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ screen = pygame.display.set_mode((400, 400))
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+def drawcell(pos, color):
+  x, y = pos
+  for i in range(x, x+20):
+    for j in range(y, y+20):
+      screen.set_at(pos, color)
+      pygame.display.update()
+
 while True:
   screen.fill(BLACK)
   screen.set_at((10, 100), WHITE)
