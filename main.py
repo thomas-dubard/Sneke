4de103b7b675bd1772807d0f8bc743bc91e05ba1
thomas-dubard <55085630+thomas-dubard@users.noreply.github.com>
2019-10-02 15:02:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ pygame.init()
 # et définir la taille de la fenêtre (400x400)
 screen = pygame.display.set_mode((400, 400))
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
 while True:
+  screen.fill(BLACK)
+  screen.set_at((10, 100), WHITE)
+  pygame.display.update()
   for event in pygame.event.get(KEYDOWN):
     if event.key == K_q:
       # on quitte le programme lors d'un appui sur Q
       sys.exit()
-
